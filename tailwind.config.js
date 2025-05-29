a22493d938f560a9fed6df0994457cf8004d08d7
etowageed <etowagideon@gmail.com>
2025-05-30 00:39:01 +0100

    initialised project

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  darkMode: "class",
+  content: [
+    "./components/**/*.{vue,js}",
+    "./layouts/**/*.{vue,js}",
+    "./pages/**/*.{vue,js}",
+    "./app.vue",
+    "./plugins/**/*.{js,ts}",
+  ],
+  theme: {
+    extend: {},
+  },
+  // plugins: [require("@tailwindcss/typography")],
+};
