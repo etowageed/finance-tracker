27f5270224473765098421b32f8f9dd63b66816c
etowageed <etowagideon@gmail.com>
2025-06-10 00:32:39 +0100

    added trends in dashboard

--- pages/index.vue
+++ pages/index.vue
@@ -16,8 +16,8 @@ const selectedView = ref(transactionViewOptions[1])
 
     <section class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 sm:gap-16 mb-10">
         <Trend color="green" title="Income" :amount="4000" :last-amount="3000" :loading="false" />
-        <Trend color="red" title="Income" :amount="4000" :last-amount="3000" :loading="false" />
-        <Trend color="green" title="Income" :amount="4000" :last-amount="3000" :loading="false" />
+        <Trend color="red" title="Expense" :amount="4000" :last-amount="6000" :loading="false" />
+        <Trend color="green" title="Investments" :amount="4000" :last-amount="3000" :loading="false" />
         <Trend color="red" title="Income" :amount="4000" :last-amount="3000" :loading="true" />
     </section>
 </template>
